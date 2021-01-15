ce0de7bbeced5361fa62283c525080b474cad66d
FlavioM4 <58825187+FlavioM4@users.noreply.github.com>
2021-01-15 19:18:02 +0000

    Add files via upload

---
+++ gamepricechecker.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+
+def getPage(gameName):
+	gameName = gameName
+	url = "https://www.allkeyshop.com/blog/catalogue/search-"+gameName+"/"
+	try:
+		page = requests.get(url)
+		if page.status_code == 200:
+			soup = BeautifulSoup(page.content, 'html.parser')
+			a = soup.find_all('div', class_="search-results-row-price")
+			price = a[0].get_text()
+			priceTag = price.split()
+			print("The lowest price you can get, right now is: "+ priceTag[0])
+		else:
+			print("Something wrong just happened, try again")
+			input()
+	except:
+		print("Problem getting the page, please check your internet connection")
+	
+
+def normalizeName(game):
+	game = game
+	gameName = ""
+	for i in game:
+		if i == " ":
+			i = "-"
+			gameName = gameName + i
+		else:
+			gameName = gameName + i
+	getPage(gameName)
+
+
+def inputName():
+	game = input("Choose the game: ")
+	normalizeName(game)
+
+
+inputName()
